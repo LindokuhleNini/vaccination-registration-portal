9252e2946a3ea313708a462ab352b0b0fcb9c26a
Lindokuhle Nini <lindokuhlepaul6@gmail.com>
2021-09-20 00:31:09 +0200

    routes created

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,40 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home');
 });
+
+router.get('/home', function(req, res, next) {
+  res.render('Home');
+});
+
+router.get('/id', function(req, res, next) {
+  res.render('IdOrPassport');
+});
+
+router.get('/idform', function(req, res, next) {
+  res.render('IdForm');
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contactDetails');
+});
+
+router.get('/address', function(req, res, next) {
+  res.render('Address');
+});
+
+router.get('/medicalaid', function(req, res, next) {
+  res.render('medicalAid');
+});
+
+router.get('/madetails', function(req, res, next) {
+  res.render('medicalAidDetails');
+});
+
+router.get('/done', function(req, res, next) {
+  res.render('successfulRegistration');
+});
+
 
 module.exports = router;
