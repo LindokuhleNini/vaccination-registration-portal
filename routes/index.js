dc78ae0dfb48ec04903b40c0525128e16f80b6a3
Thapelo <56066066+Yizo-Yizo@users.noreply.github.com>
2021-09-21 15:13:00 +0200

    Separate css from hmtl

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+// middleware & static files
+app.use(express.static('public'));
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('home');
